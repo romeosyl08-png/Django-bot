62c39935086f9f8fc010981a07230bdd98e5146c
romeosyl08-png <romeosyl08@gmail.com>
2026-06-06 21:50:47 +0000

    requirements commit

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4yua#%814!whhptfhua6gz=jquspikyhizqhhe(yig$5hc-r8m'
+SECRET_KEY = os.environ.get('SECRET_KEY')  # Utilisez une vraie clé secrète en production
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
